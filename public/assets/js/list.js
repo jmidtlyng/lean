e9244e6b17f8b83c7956e864d5e3f44ad25f306c
Jacob Midtlyng <>
2020-11-17 20:55:48 -0800

    standard search opens quickview

--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -1,9 +1,67 @@
+const h = gridjs.h;
+
 const grid = {
   init(tableId, wrapperId){
     const tableRef = document.getElementById(tableId),
-          wrapperRef = document.getElementById(wrapperId);
+          el_wrapper = document.getElementById(wrapperId);
 
-    const grid = new gridjs.Grid({ from: tableRef, sort: true }).render(wrapperRef)
-    grid.on("ready", (...args) => htmx.process(wrapperRef));
+    const grid = new gridjs.Grid({ from: tableRef, sort: true }).render(el_wrapper)
+    grid.on("ready", (...args) => htmx.process(el_wrapper));
+  }
+}
+
+const gridServerSide = {
+  init(tableId, wrapperId, entityType){
+    const el_table = document.getElementById(tableId),
+          el_wrapper = document.getElementById(wrapperId),
+          url = "/_json?type=" + entityType;
+    const grid = new gridjs.Grid({
+      from: el_table,
+      server: { url: url,
+        then: data => {
+          return data.map(row => {
+            row.name = h('div', {class: "flex"},
+              h('span', {
+                  "hx-target": "#listResultsQuickview",
+                  "hx-swap": "innerHTML",
+                  "hx-trigger": "click",
+                  "hx-get": ("/components/quickview/_single/"+row.entity)},
+                h('img', {class:"search-results-quickview-icon", src:"/icons/binos.svg"}, null)
+              ),
+              h('span', {}, row.name));
+            return Object.values(row)
+          })
+        }
+      },
+      pagination: {
+        limit: 50,
+        server: {url: (prev, page, limit) => `${prev}&limit=${limit}&offset=${page * limit}`}
+      },
+      sort: {
+        multiColumn: true,
+        server: {
+          url: (prev, columns) => {
+            if (!columns.length) return prev;
+
+            const el_cols = el_table.getElementsByTagName('th');
+            let sorts = [],
+                dirs = [];
+
+            for(const col of columns){
+              sorts.push( el_cols[col.index].getAttribute("name") );
+              dirs.push(col.direction);
+            }
+
+            return `${prev}&sort=${sorts}&dir=${dirs}`;
+          }
+        },
+      },
+      search: {
+        server: { url: (prev, keyword) => `${prev}&search=${keyword}` }
+      },
+    });
+
+    grid.render(el_wrapper);
+    grid.on("ready", (...args) => htmx.process(el_wrapper));
   }
 }
